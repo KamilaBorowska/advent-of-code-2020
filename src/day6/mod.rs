d3f597e1b812197dcfb7237fcc6f0f79874ce45a
Konrad Borowski <konrad@borowski.pw>
2020-12-06 12:27:15 +0100

    Day 6

---
+++ src/day6/mod.rs
@@ -0,0 +1,76 @@
+use crate::Solution;
+use std::collections::HashSet;
+
+pub(super) const DAY6: Solution = Solution {
+    part1: |input| {
+        let sum: usize = input
+            .split("\n\n")
+            .map(|answers| {
+                answers
+                    .chars()
+                    .filter(|c| c.is_alphabetic())
+                    .collect::<HashSet<_>>()
+                    .len()
+            })
+            .sum();
+        Ok(sum.to_string())
+    },
+    part2: |input| {
+        let sum: usize = input
+            .split("\n\n")
+            .map(|answers| {
+                let mut split_answers = answers.lines();
+                let mut first: HashSet<char> = if let Some(answer) = split_answers.next() {
+                    answer.chars().collect()
+                } else {
+                    return 0;
+                };
+                for answer in split_answers {
+                    let other_set: HashSet<char> = answer.chars().collect();
+                    first.retain(|elem| other_set.contains(elem));
+                }
+                first.len()
+            })
+            .sum();
+        Ok(sum.to_string())
+    },
+};
+
+#[cfg(test)]
+mod test {
+    use crate::{lines, test};
+    const EXAMPLE2: &str = lines!(
+        "abc"
+        ""
+        "a"
+        "b"
+        "c"
+        ""
+        "ab"
+        "ac"
+        ""
+        "a"
+        "a"
+        "a"
+        "a"
+        ""
+        "b"
+    );
+    test!(
+        DAY6.part1,
+        empty: "" => 0,
+        example1: lines!(
+            "abcx"
+            "abcy"
+            "abcz"
+        ) => 6,
+        example2: EXAMPLE2 => 11,
+        input: 6763,
+    );
+    test!(
+        DAY6.part2,
+        empty: "" => 0,
+        example2: EXAMPLE2 => 6,
+        input: 3512,
+    );
+}
